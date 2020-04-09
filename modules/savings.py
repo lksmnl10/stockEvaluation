e249e2282222b2f7b7a131a59048fd52ae28f35f
lksmnl10 <lksmnl10@users.noreply.github.com>
2020-04-09 00:13:39 -0600

    ADJUSTED files

--- modules/savings.py
+++ modules/savings.py
@@ -1,2 +1,3 @@
 import essentials, credit, debit, stockPortfolio
 #--------------------------------------------------------------------------------------------------------------------------------------------
+template=["Date","Category","CN","101SA","SIMPLII_SA","RSP","EQ_101","EQ-E-SA","WS-CASH",]
